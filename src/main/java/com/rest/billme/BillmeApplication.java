568acfada5a100396987896fcee8319517843bab
Artur Preys <artur.preys@4finance.com>
2019-10-24 10:44:57 +0300

    Bill-1 spring initialization

---
+++ src/main/java/com/rest/billme/BillmeApplication.java
@@ -0,0 +1,13 @@
+package com.rest.billme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillmeApplication.class, args);
+	}
+
+}
